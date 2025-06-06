62cef17fe34df6b219cce5d9f6337345d5d326d1
AustinEPL <edgepecho@gmail.com>
2025-06-05 20:46:54 -0500

    creacion de pagina de login

---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,73 @@
+import React, { useState } from 'react';
+
+const Login = () => {
+  const [usuario, setUsuario] = useState('');
+  const [contrasena, setContrasena] = useState('');
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:9999/Auth.php', {
+        method: 'POST',
+         mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          "usuario": usuario,
+          "password": contrasena
+        })
+      });
+
+      const data = await response.json();
+
+      if (response.ok && data.token) {
+        // Guarda el token y datos del usuario
+        localStorage.setItem('token', data.token);
+        localStorage.setItem('usuario', JSON.stringify(data.usuario));
+        localStorage.setItem('rol', JSON.stringify(data.rol));
+
+
+        console.log('Autenticación exitosa');
+        // Aquí puedes redirigir o cambiar de vista
+      } else {
+        setError(data.error || 'Error en la autenticación');
+      }
+    } catch (err) {
+      console.error('Error al conectar con el backend:', err);
+      setError('No se pudo conectar al servidor');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>Usuario:</label>
+        <input
+          type="text"
+          value={usuario}
+          onChange={(e) => setUsuario(e.target.value)}
+          required
+        />
+      </div>
+
+      <div>
+        <label>Contraseña:</label>
+        <input
+          type="password"
+          value={contrasena}
+          onChange={(e) => setContrasena(e.target.value)}
+          required
+        />
+      </div>
+
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+
+      <button type="submit">Iniciar sesión</button>
+    </form>
+  );
+};
+
+export default Login;
